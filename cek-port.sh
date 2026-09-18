2e2bad826bddc457f8c81fe046c47bd1be3117f7
kayu55 <iwanles109@gmail.com>
2026-09-18 18:53:34 +0700

    Add files via upload

--- cek-port.sh
+++ cek-port.sh
@@ -5,21 +5,13 @@ server_order=(
   "SG-VIP"
   "SG-VVIP"
   "SG-VVIP2"
-  "SG-TEAM1"
-  "ID-NUSA"
-  "ID-NUSA2"
-  "ID-NUSA-STB"
 )
 
 # Alias => domain
 declare -A servers=(
-  ["SG-VIP"]="sgvip.rajaserverpremium.web.id"
-  ["SG-VVIP"]="sgvvip.rajaserverpremium.web.id"
-  ["SG-VVIP2"]="sgvvip2.rajaserverpremium.web.id"
-  ["SG-TEAM1"]="sgteam1.rajaserverpremium.web.id"
-  ["ID-NUSA"]="idnusa.rajaserverpremium.web.id"
-  ["ID-NUSA2"]="idnusa2.rajaserverpremium.web.id"
-  ["ID-NUSA-STB"]="idnusastb.rajaserverpremium.web.id"
+  ["SG-VIP"]="94.237.65.134"
+  ["SG-VVIP"]="95.111.199.29"
+  ["SG-VVIP2"]="172.237.87.184"
 )
 
 # Port + label
